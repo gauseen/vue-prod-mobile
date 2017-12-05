d2016b6f13d338dd9f15308e5f5a34bda3af504e
gcc <gavin_008@163.com>
2017-12-05 14:19:16 +0800

    assets 背景图片测试

--- src/pages/goods/index.vue
+++ src/pages/goods/index.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="test__content">
-
+		<div class="child__one"></div>
+		<div class="child__two"></div>
 	</div>
 </template>
 
@@ -13,12 +14,16 @@ export default {
 
 <style lang="stylus" scoped>
 .test__content
-	width auto
-	height 370px
-	border 1px solid red
-	// assets文件夹会经过webpack处理，static 文件夹 webpack不会处理
-	background url("../../assets/img/card_skin_1.jpg") no-repeat center
-	background-size convert
+	> div
+		width auto
+		height 370px
+		border 1px solid red
+		background-size convert
+	.child__one
+		// assets文件夹会经过webpack处理，static 文件夹 webpack不会处理
+		background url("../../assets/img/card_skin_1.jpg") no-repeat center
+	.child__two
+		background url("../../assets/images/card_skin.jpg") no-repeat center
 </style>
 
 
